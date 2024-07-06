344edf14275491aa21b96338de0ed00e9ba8281c
Razer4m <tumelomorakile@gmail.com>
2024-07-06 18:57:23 +0200

    Taks 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install an especific version of flask (2.1.0)
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+#!/usr/bin/pup
+# Install flask from pip3
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
