884d68180b82d018aa981d3aaaaa04f23c81daf6
Razer4m <tumelomorakile@gmail.com>
2024-07-28 00:36:50 +0200

    update on adv task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,23 @@
 # Puppet manifest to install and configure Nginx
-class { 'nginx': }
-
-# Ensure the nginx service is running
-service { 'nginx':
-  ensure => running,
-  enable => true,
+exec { 'dist update':
+        command  => '/usr/bin/apt-get update',
+        provider => 'shell'
 }
 
-# Create the root HTML page
-file { '/var/www/html/index.html':
-  ensure  => file,
+file {'/var/www/html/index.html':
+  path    => '/var/www/html/index.html',
   content => 'Hello World!',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '7624'
 }
 
-# Configure the default site with a 301 redirect
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  notify  => Service['nginx'], # Notify nginx to reload configuration
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['dist update']
 }
 
-# Template for the default site configuration
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
 }
