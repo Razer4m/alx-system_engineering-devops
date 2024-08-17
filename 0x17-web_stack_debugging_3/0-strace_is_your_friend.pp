f4345b11c2409a5959c690976c9ee7253f820f30
Razer4m <tumelomorakile@gmail.com>
2024-08-17 02:07:34 +0200

    Updates

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to fix Apache 500 Internal Server Error
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
